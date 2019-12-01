1d40f649742c6953e5353696c19db3381be6672c
Anuradha <akw8888@gmail.com>
2019-12-01 19:18:15 +0530

    Label changed for the search function

--- src/main/java/com/mit/sample/MyUI.java
+++ src/main/java/com/mit/sample/MyUI.java
@@ -43,7 +43,7 @@ public class MyUI extends UI {
         VerticalLayout logos = new VerticalLayout(logoLable);
         logos.setWidth("100%");
         logos.setHeight("10px");
-        TextField filter = new TextField("search from hostname");
+        TextField filter = new TextField("search from HostName");
         VerticalLayout actions = new VerticalLayout();
 
         GridLayout action = new GridLayout(2, 1);
